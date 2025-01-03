e9cc5e8c20dfe5889882ab9b98eb6597bcf0612f
Oleh_Babiak <oleh_babiak@epam.com>
2025-01-03 16:21:27 +0200

    added state for saving payer name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const input = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleSubmit() {
+    setEnteredPlayerName(input.current.value)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName || 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={input} type="text" />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
