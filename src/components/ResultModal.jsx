1fc7de772260d9236609654337f823f14421f77f
Oleh_Babiak <oleh_babiak@epam.com>
2025-01-15 13:58:01 +0200

    using useImperativeHandle, External components only have access to the required methods

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
 // export default function ResultModal({ ref, result, targetTime }) {
 //     return (
-//         <dialog ref={ref} className="result-modal" open>
+//         <dialog ref={ref} className="result-modal">
 //             <h2>Your {result}</h2>
 //             <p>The target time was <strong>{targetTime} seconds.</strong></p>
 //             <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -11,11 +11,20 @@
 //     );
 // }
 
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal()
+            }
+        };
+    });
     return (
-        <dialog ref={ref} className="result-modal" open>
+        <dialog ref={dialog} className="result-modal">
             <h2>Your {result}</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
